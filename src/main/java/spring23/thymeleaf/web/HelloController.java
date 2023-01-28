3b3c42ac0c288e453cc78c0702a3d97fa55b85a3
espu <112239289+espu@users.noreply.github.com>
2023-01-28 12:11:09 +0200

    First commit

---
+++ src/main/java/spring23/thymeleaf/web/HelloController.java
@@ -0,0 +1,18 @@
+package spring23.thymeleaf.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class HelloController {
+	@RequestMapping(value="/hello", method=RequestMethod.GET)
+	public String welcomeMsg(@RequestParam (name="name") String name, @RequestParam (name="age") int age, Model model) {
+		model.addAttribute("name", name);
+		model.addAttribute("age", age);
+		return "hello";
+	}
+}
---
+++ src/test/java/spring23/thymeleaf/ThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package spring23.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
